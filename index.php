b0605dc1a70f2df52f3474565130672cbb1a1407
Дени <denninnka@localhost.localdomain>
2016-12-08 15:52:23 +0200

    chenge formating

--- index.php
+++ index.php
@@ -1,30 +1,29 @@
 <?php
-$pageTitle='Списък разходи';
+$pageTitle = 'Списък разходи';
 include './includes/header.php';
-$selectedGroup=-1;
+$selectedGroup = -1;
 var_dump($_GET['group']);
 
 if (isset($_GET['dategroup']) && !empty($_GET['dategroup'])) {
-	$selectedDategroup=$_GET['dategroup'];
-}
-else{
-	$selectedDategroup=-1;
+    $selectedDategroup = $_GET['dategroup'];
+} else {
+    $selectedDategroup = -1;
 }
 
 if ($_GET) {
-	$selectedGroup=(int)$_GET['group'];
+    $selectedGroup = (int) $_GET['group'];
 }
-if($_POST){
-	if (file_exists('./date.txt')) {
-		$result=file('./date.txt');
+if ($_POST) {
+    if (file_exists('./date.txt')) {
+        $result = file('./date.txt');
 
-		foreach ($_POST['delete'] as $value) {
-			unset($result[(int)$value]);
-		}
-		if(file_put_contents('./date.txt', implode('', $result))){
-			echo 'Записът е изтрит успешно';
-		}	
-	}
+        foreach ($_POST['delete'] as $value) {
+            unset($result[(int) $value]);
+        }
+        if (file_put_contents('./date.txt', implode('', $result))) {
+            echo 'Записът е изтрит успешно';
+        }
+    }
 }
 
 ?>
@@ -34,10 +33,10 @@ if($_POST){
 <select name="group">
 	<option value="-1">Всички</option>
 	<?php
-	foreach ($groups as $key => $value) {
-		echo '<option value="'.$key.'" '.($key == $selectedGroup ? 'selected':'').'>' .$value. '</option>';
-	}
-	?>	
+foreach ($groups as $key => $value) {
+    echo '<option value="' . $key . '" ' . ($key == $selectedGroup ? 'selected' : '') . '>' . $value . '</option>';
+}
+?>
 </select>
 <input type="date" name="dategroup">
 <input type="submit" value="Филтрирай" />
@@ -54,39 +53,39 @@ if($_POST){
 	</tr>
 
 	<?php
-	if (file_exists('./date.txt')) {
-		$result=file('./date.txt');
-		$sum=0;
-		foreach ($result as $k => $value) {
-			
-			$columns=explode('!', ($value));
+if (file_exists('./date.txt')) {
+    $result = file('./date.txt');
+    $sum    = 0;
+    foreach ($result as $k => $value) {
 
-			$date = $columns[0];
-			$name = $columns[1];
-			$amount = $columns[2];
-			$group = $columns[3];
+        $columns = explode('!', ($value));
 
-			if (($selectedGroup==-1 && $selectedDategroup==-1) || ($selectedGroup==-1 && $selectedDategroup==$date) || ($selectedGroup==$group && $selectedDategroup==-1) || ($selectedGroup==$group && $selectedDategroup==$date)) {
-			
-				echo'<tr>
-					<td>' .$date.'</td>
-					<td>' .$name.'</td>
-					<td>' .$amount.'</td>
-					<td>' .$groups[trim($group)].'</td>
-					<td><a href="/Razhod/edit.php?red='.$k.'">Редактирай</a></td>
-					<td><input type="checkbox" name="delete[]" value="'.$k.'"></td>
+        $date   = $columns[0];
+        $name   = $columns[1];
+        $amount = $columns[2];
+        $group  = $columns[3];
+
+        if (($selectedGroup == -1 && $selectedDategroup == -1) || ($selectedGroup == -1 && $selectedDategroup == $date) || ($selectedGroup == $group && $selectedDategroup == -1) || ($selectedGroup == $group && $selectedDategroup == $date)) {
+
+            echo '<tr>
+					<td>' . $date . '</td>
+					<td>' . $name . '</td>
+					<td>' . $amount . '</td>
+					<td>' . $groups[trim($group)] . '</td>
+					<td><a href="/Razhod/edit.php?red=' . $k . '">Редактирай</a></td>
+					<td><input type="checkbox" name="delete[]" value="' . $k . '"></td>
 				</tr>';
-				$sum  += $amount;
-			}
-		}
+            $sum += $amount;
+        }
+    }
 //var_dump($result);
-	}
-	?>
+}
+?>
 
 	<tr>
 			<td>--</td>
 			<td>--</td>
-			<td><?= $sum; ?></td>
+			<td><?=$sum;?></td>
 			<td>--</td>
 			<td>--</td>
 			<td>--</td>
